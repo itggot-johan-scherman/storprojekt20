1427086cd123ac11e7e22fdd29435d33aa50c902
itggot-johan-scherman <48438397+itggot-johan-scherman@users.noreply.github.com>
2020-02-06 15:44:32 +0100

    you can now almost write a review

--- code/app.rb
+++ code/app.rb
@@ -1,19 +1,23 @@
 require 'sinatra'
 require 'slim'
 require 'sqlite3'
-require 'BCrypt'
+require 'bcrypt'
 enable :sessions
+
+def connect_db(route)
+    db = SQLite3::Database.new(route)
+    db.results_as_hash = true
+end
 
 get("/") do
     slim(:index)
 end
 
-post("/reviews/") do
+post("/reviews") do
     username = params[:username]
     password = params[:password]
     password_digest = BCrypt::Password.create(password)
-    db = SQLite3::Database.new("db/tododatabase.db")
-    db.results_as_hash = true
+    db = connect_db("db/database.db")
     user_check = db.execute("SELECT id FROM users WHERE username = ?", username)
     password_check = db.execute("SELECT password_digest FROM users WHERE username = ?", username)
     if user_check != nil
@@ -28,4 +32,21 @@ post("/reviews/") do
         set_error("User does not exist")
         redirect("/error")
     end
+end
+
+post("/new") do
+ #   db = connect_db("db/database.db")
+  #  title = params[:title]
+   # title_check = db.execute("SELECT title FROM titles")
+    #if title_check.scan(/#{title}/).empty?
+     #   db.execute("INSERT INTO titles title = ?", title)
+    #end
+#    genre = params[:genre]
+ #   genre_check = db.execute("SELECT title FROM titles")
+  #  if genre_check.scan(/#{genre}/).empty?
+   #     db.execute("INSERT INTO genres genre = ?", genre)
+    #end
+ #   review = params[:review]
+  #  db.execute("INSERT INTO reviews content = ?", review)
+   # redirect("/reviews")
 end
